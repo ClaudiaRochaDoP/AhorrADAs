bc61de93981f183c3cf898fea0ecb06f3805bddc
claudiarochadop <claudiarochadop@gmail.com>
2024-08-27 22:53:39 -0500

    empezando funcionalidad de botones para mostrar secciones

--- main.js
+++ main.js
@@ -6,22 +6,41 @@ const menuHamburguesa = document.getElementById("menu-hamburguesa");
   menuHamburguesa.classList.toggle('hidden');
 });*/
 
-
 // Mostrar y ocultar filtros
 
-const filtros = document.getElementById('filtros');
-const alternarFiltros = document.getElementById('alternar-filtros');
+const filtros = document.getElementById("filtros");
+const alternarFiltros = document.getElementById("alternar-filtros");
 
-function mostrarOcultarFiltros () {
-  if (filtros.classList.contains('hidden')) {
-    alternarFiltros.innerText = 'Ocultar Filtros';
-    filtros.classList.remove('hidden');
+function mostrarOcultarFiltros() {
+  if (filtros.classList.contains("hidden")) {
+    alternarFiltros.innerText = "Ocultar Filtros";
+    filtros.classList.remove("hidden");
   } else {
-    alternarFiltros.innerText = 'Mostrar filtros';
-    filtros.classList.add('hidden');
+    alternarFiltros.innerText = "Mostrar filtros";
+    filtros.classList.add("hidden");
   }
 }
 
-alternarFiltros.addEventListener('click', function(){
+alternarFiltros.addEventListener("click", function () {
   mostrarOcultarFiltros();
-})
+});
+
+/* ----------------------- se muestran los paneles ---------------------*/
+const sectionBalance = document.getElementById("section-balance");
+const sectionCAtergporiasil = document.getElementById("section-categorias");
+const sectionOperaciones = document.getElementById("section-operaciones");
+
+/* ----------------------- botones menu de hamburguesa ---------------------*/
+const botonBalanceHamburguesa = document.getElementById(
+  "boton-balance-hamburguesa"
+);
+const botonCategoriaHamburguesa = document.getElementById(
+  "boton-categoria-hamburguesa"
+);
+const botonReportesHamburguesa = document.getElementById(
+  "boton-reportes-hamburguesa"
+);
+
+botonBalanceHamburguesa.addEventListener("click", function () {
+  sectionFiltros.classList.remove("hidden");
+});
